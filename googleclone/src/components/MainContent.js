368d9d6899ed5221a39af15c23b722fcfae05ea3
JSNavey <jsnavey@gmail.com>
2018-10-21 00:28:59 -0400

    styling main content and footer add google logo in main contain add background color to footer

--- googleclone/src/components/MainContent.js
+++ googleclone/src/components/MainContent.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 class MainContent extends Component {
   render() {
     return (
-      <div>
-        MainContent
+      <div className='main-container'>
+        <img className='google-logo' src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png' />
+        
       </div>
     );
   }
