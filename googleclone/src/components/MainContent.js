d348d7890d7d93d3513b73e102e67e05c44d40e3
JSNavey <jsnavey@gmail.com>
2018-10-21 15:24:08 -0400

    adjusting the size and position of search bar and buttons

--- googleclone/src/components/MainContent.js
+++ googleclone/src/components/MainContent.js
@@ -4,12 +4,14 @@ class MainContent extends Component {
   render() {
     return (
       <div className='main-container'>
-        <img className='google-logo' src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png' />
-        <div className='search-area'>
-          <input className='search-input' />
-          <div className='search-btn'>
-            <button>Google Search</button>
-            <button>I'm Feeling Lucky</button>
+        <div className='middle-container'>
+          <img className='google-logo' src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png' />
+          <div className='search-area'>
+            <input className='search-input' />
+            <div className='search-btn'>
+              <button>Google Search</button>
+              <button>I'm Feeling Lucky</button>
+            </div>
           </div>
         </div>
       </div>
