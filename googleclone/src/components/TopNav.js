74a9f07144dbd7a1e443680df359f3622fec7113
JSNavey <jsnavey@gmail.com>
2018-10-20 23:59:17 -0400

    create boilerplate"

---
+++ googleclone/src/components/TopNav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TopNav extends Component {
+  render() {
+    return (
+      <div>
+        TopNav
+      </div>
+    );
+  }
+}
+
+export default TopNav;
